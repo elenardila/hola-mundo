3c59104e268efc0254335ee560640891ce839c0e
elenardila <eardilad01@iesalbarregas.es>
2025-10-01 10:41:21 +0200

    feat: cambios en back (Spring Boot)

--- src/main/java/es/ejemplo/hola_mundo/controller/TestController.java
+++ src/main/java/es/ejemplo/hola_mundo/controller/TestController.java
@@ -2,11 +2,12 @@ package es.ejemplo.hola_mundo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.Map;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TestController {
 
 
